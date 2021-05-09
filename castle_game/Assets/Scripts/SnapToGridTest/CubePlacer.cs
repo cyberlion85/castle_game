eef007a94075afa5630dce54ad2416600a4bb09d
Stanislav Persanov <s.persanov@gmail.com>
2021-05-09 17:03:13 +0300

    placing 2 prefabs on grid, button select, raycast mouse follow

---
+++ castle_game/Assets/Scripts/SnapToGridTest/CubePlacer.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+
+public class CubePlacer : MonoBehaviour
+{
+    private Grid grid;
+    public GameObject prefab1;
+    public GameObject prefab2;
+    private GameObject inst_obj;
+    //public GameObject showingObj;
+    [SerializeField] GameObject activePrefab;
+
+    private void Awake()
+    {
+        grid = FindObjectOfType<Grid>();
+        activePrefab = prefab1;
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Debug.Log("call GetMouseButtonDown");
+
+            RaycastHit hitInfo;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                PlaceCubeNear(hitInfo.point);
+            }
+
+        }
+        RaycastHit[] hits;
+        Ray rayMouse;
+
+        rayMouse = Camera.main.ScreenPointToRay(Input.mousePosition);
+        hits = Physics.RaycastAll(rayMouse);
+        int i = 0;
+        while (i < hits.Length)
+        {
+            RaycastHit hit = hits[i];
+            Debug.Log(hit.collider.gameObject.name);
+            i++;
+
+            if (hit.collider.gameObject.name == "Plane")
+            {
+                inst_obj.transform.position = new Vector3(hit.point.x, 0.1f, hit.point.z);
+            }
+        }
+        }
+
+
+    void PlaceCubeNear(Vector3 clickPoint)
+    {
+        Vector3 finalPosition = grid.GetNearestPointOnGrid(clickPoint);
+        /*        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.transform.position = finalPosition;
+                cube.GetComponent<Renderer>().material.color = Color.blue;*/
+
+        Instantiate(activePrefab, finalPosition, Quaternion.identity);
+    }
+    //TODO peredelat na spisok  
+     void SetPrefab1()
+        {
+        activePrefab=prefab1;
+        SetCursorObj(activePrefab);
+    }
+    void SetPrefab2()
+        {
+        activePrefab=prefab2;
+        SetCursorObj(activePrefab);
+    }
+
+    void SetCursorObj(GameObject obj)
+    {
+        Destroy(inst_obj);
+        inst_obj = Instantiate(obj, transform.position, Quaternion.identity);
+    }
+
+}
---
+++ castle_game/Assets/Scripts/SnapToGridTest/Grid.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    [SerializeField]
+    private float size = 1f;
+
+    public Vector3 GetNearestPointOnGrid(Vector3 clickPoint)
+    {
+        Debug.Log("call GetNearestPointOnGrid");
+        //Debug.Log(clickPoint);
+        Debug.Log(transform.position);
+        //GameObject.CreatePrimitive(PrimitiveType.Sphere).transform.position = transform.position;
+
+        //clickPoint -= transform.position;
+
+        int xCount = Mathf.RoundToInt(clickPoint.x / size);
+        int yCount = Mathf.RoundToInt(clickPoint.y / size);
+        int zCount = Mathf.RoundToInt(clickPoint.z / size);
+
+        Vector3 result = new Vector3(
+            (float)xCount * size,
+            (float)yCount * size,
+            (float)zCount * size);
+
+        result += transform.position;
+
+        return result;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        for (float x = 0; x < 40; x += size)
+        {
+            for (float z = 0; z < 40; z += size)
+            {
+                var point = GetNearestPointOnGrid(new Vector3(x, 0f, z));
+                Gizmos.DrawSphere(point, 0.1f);
+            }
+                
+        }
+    }
+}
